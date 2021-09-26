bbed07808be9a39c4fc5c485ddcabeb3e27ca0af
klkokou <darya26zapuskalova0202@gmail.com>
2021-09-26 12:40:07 +0300

    trying to get rid of .idea/

---
+++ README.md
@@ -0,0 +1,1 @@
+[![codecov](https://codecov.io/gh/klkokou/.NET-homework/branch/2k-164/graph/badge.svg?token=EXRDQNJ1JN)](https://codecov.io/gh/klkokou/.NET-homework)
---
+++ src/Homework1/Homework1/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework1
+{
+    public static class Calculator
+    {
+        public enum SupportedOperations
+        {
+            Plus,
+            Minus,
+            Divide,
+            Multiply,
+        }
+
+        public static double Calculate(int val1, SupportedOperations operation, int val2)
+        {
+            var result = 0.0;
+            switch (operation)
+            {
+                case SupportedOperations.Plus:
+                    result = val1 + val2;
+                    break;
+                case SupportedOperations.Minus:
+                    result = val1 - val2;
+                    break;
+                case SupportedOperations.Multiply:
+                    result = val1 * val2;
+                    break;
+                case SupportedOperations.Divide:
+                    result = (double)val1 / val2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
