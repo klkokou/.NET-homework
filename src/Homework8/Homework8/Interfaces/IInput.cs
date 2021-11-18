a39ea390a4e79ee4fc9b61c7b825017c69e35219
klkokou <darya26zapuskalova0202@gmail.com>
2021-11-18 09:05:58 +0300

    DI homework

---
+++ src/Homework8/Homework8/Interfaces/IInput.cs
@@ -0,0 +1,12 @@
+﻿using Homework8.Parser;
+
+namespace Homework8.Interfaces
+{
+    public interface IInput
+    {
+        decimal Value1 { get; set; }
+        SupportedOperations Operation { get; set; }
+        decimal Value2 { get; set; }
+        string ErrorMessage { get; set; }
+    }
+}
---
+++ src/Homework8/Homework8/Interfaces/IParser.cs
@@ -0,0 +1,7 @@
+﻿namespace Homework8.Interfaces
+{
+    public interface IParser
+    {
+        void ParseArgs(string val1, string operation, string val2, ref IInput input);
+    }
+}
